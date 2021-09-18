8e0671604af2e0b4d90b934debf19b42e4a1b2f9
TheThomasY <twy1g16@soton.ac.uk>
2021-09-18 15:35:41 +0100

    style: transition and show/hide added to share panel

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
+document
+  .getElementById('show-share-panel')
+  .addEventListener('click', function () {
+    if (mediaWidth.matches) {
+      document.getElementById('share-panel-mobile').classList.add('show');
+    } else console.log('ELSE:', mediaWidth);
+  });
 
+document
+  .getElementById('hide-share-panel')
+  .addEventListener('click', function () {
+    document.getElementById('share-panel-mobile').classList.remove('show');
+  });
+
+let mediaWidth = window.matchMedia('(max-width: 879px)');
